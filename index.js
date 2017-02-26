6f4000607c606b1915499df21361d8461f20983e
Oskar <rahlenoskar@gmail.com>
2017-02-26 21:26:28 +0100

    yeee
    U2CGX51J8

--- index.js
+++ index.js
@@ -4,72 +4,97 @@ var request = require('request');
 var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-var payload = {
-    'text':'YEEE',
-    'username': 'Mr slack bot',
-    'icon_emoji': ':nerd_face:'
-}
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var url = 'https://hooks.slack.com/services/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'
-var options = {
-  method: 'post',
-  body: payload,
-  json: true,
-  url: url
-}
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(reques, response) {
-  response.render('pages/index');
-
-});
-
 var clientId = "143457452320.144253511221";
 var clientSecret = "fdfb5b7fedfc81dca623f06e3e813a4b";
-app.get('/callback', function(reques, responsee) {
+var token = "xoxp-143457452320-144242893812-145736613559-d07b5882d8ba48a507bdff287ec7f5f6";
 
+app.get('/', function(reques, response) {
+  response.render('pages/index');
+});
+
+app.get('/callback', function(reques, responsee) {
   var code = reques.query.code;
-  var redirectString = 'http://slack.com/api/oauth.access?client_id=' +clientId + '&client_secret=' + clientSecret + '&code=' + code;
-  var token = "hej";
-  request(redirectString, function(error,response,body){
-    var jsonBody = JSON.parse(body);
-    token = jsonBody["access_token"];
-  //  console.log(body);
+
+  /*
+  getToken(code).then(function(tokenm){
+    return getIM()
+  }).then(function(users){
+    console.log(users[2].id);
+    return sendDirectMessage(users[2].id, 'weeey!!')
+  }).then(function(response){
+    responsee.send(response);
+  }).catch(function (error) {
+     console.log(error);
+   });
+   */
+   
+});
+
+
+var getToken = function(code){
+  return new Promise(function(resolve, reject) {
+    var redirectString = 'http://slack.com/api/oauth.access?client_id=' +clientId + '&client_secret=' + clientSecret + '&code=' + code;
+    request(redirectString, function(error,response,body){
+      var jsonBody = JSON.parse(body);
+      if(error != null){
+        reject(error);
+      }
+
+      token = jsonBody["access_token"];
+      resolve(token);
+    });
+  });
+}
+
+var sendDirectMessage = function(channel, text){
+  return new Promise(function(resolve, reject){
+    var sendDMMessageString = 'https://slack.com/api/chat.postMessage?token=' + token + '&channel=' + channel + '&text=' + text + '&as_user=true'  ;
+    request(sendDMMessageString, function(error, response, body){
+      if(error != null){
+        reject(error);
+      }
+
+      var jsonBody = JSON.parse(body);
+      resolve(jsonBody);
+    });
+  });
+}
+
+var getIM = function(){
+  return new Promise(function(resolve, reject){
+    var groupString = 'https://slack.com/api/im.list?token=' + token;
+    request(groupString, function(error, response, body){
+      if(error != null){
+        reject(error);
+      }
+      var jsonBody = JSON.parse(body);
+      var ims = jsonBody["ims"];
+      resolve(ims);
+    });
+  });
+}
+
+var getUsers = function(){
+  return new Promise(function(resolve, reject){
     var groupString = 'https://slack.com/api/users.list?token=' + token;
     request(groupString, function(error, response, body){
-      console.log(body + " hejk");
+      if(error != null){
+        reject(error);
+      }
+
+      var jsonBody = JSON.parse(body);
+      var users = jsonBody["members"];
+      resolve(users);
     });
-
-    responsee.redirect(groupString);
-  });
-
-
-});
-
-/*
-app.post('/', function(req, res) {
-  payload.text = req.body.name;
-  payload.username = req.body.user;
-  options.url = req.body.chat;
-
-  console.log(req.body);
-  request(options, function (err, res, body) {
-    console.log(body);
-  });
-  res.render('pages/index');
-
-
-});
-
-*/
+  })
+}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
