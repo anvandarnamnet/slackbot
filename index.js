849a60e39ffe97f681a5fd946f9bc0dd8d4ee3f7
Oskar Råhén <rahlenoskar@gmail.com>
2017-02-27 21:17:23 +0100

    hej

--- index.js
+++ index.js
@@ -13,14 +13,21 @@ app.set('view engine', 'ejs');
 
 var clientId = "143457452320.144253511221";
 var clientSecret = "fdfb5b7fedfc81dca623f06e3e813a4b";
-var token = "xoxp-143457452320-144242893812-145736613559-d07b5882d8ba48a507bdff287ec7f5f6";
+var token;
 
 app.get('/', function(reques, response) {
+  // check if there is any token. If == 1, display it. if == 0, hook to slack; if > 1 show a list with
+  // the channels
   response.render('pages/index');
 });
 
 app.get('/callback', function(reques, responsee) {
   var code = reques.query.code;
+
+  getToken(code).then(function(tokenm){
+    token = tokenm;
+    response.render('pages/index');
+  });
 
   /*
   getToken(code).then(function(tokenm){
@@ -33,26 +40,35 @@ app.get('/callback', function(reques, responsee) {
   }).catch(function (error) {
      console.log(error);
    });
-   */
-   
+  */
+
 });
 
 
+// get a token based on the code recieved from the slack oAtuh and the apps client id and client secret.
 var getToken = function(code){
   return new Promise(function(resolve, reject) {
-    var redirectString = 'http://slack.com/api/oauth.access?client_id=' +clientId + '&client_secret=' + clientSecret + '&code=' + code;
-    request(redirectString, function(error,response,body){
+
+    // the question string to slack
+    var questionString = 'http://slack.com/api/oauth.access?client_id=' + clientId + '&client_secret=' + clientSecret + '&code=' + code;
+    request(questionString, function(error,response,body){
       var jsonBody = JSON.parse(body);
       if(error != null){
         reject(error);
       }
 
       token = jsonBody["access_token"];
+
+      // return the token
       resolve(token);
     });
   });
 }
 
+
+// send a direct message to somone in the slack channel
+// @channel - the channel id for the direct message
+// @text - the text to send
 var sendDirectMessage = function(channel, text){
   return new Promise(function(resolve, reject){
     var sendDMMessageString = 'https://slack.com/api/chat.postMessage?token=' + token + '&channel=' + channel + '&text=' + text + '&as_user=true'  ;
@@ -62,11 +78,15 @@ var sendDirectMessage = function(channel, text){
       }
 
       var jsonBody = JSON.parse(body);
+      // return the json body
       resolve(jsonBody);
     });
   });
 }
 
+
+
+// get all the IM ids for direct messages
 var getIM = function(){
   return new Promise(function(resolve, reject){
     var groupString = 'https://slack.com/api/im.list?token=' + token;
@@ -76,11 +96,14 @@ var getIM = function(){
       }
       var jsonBody = JSON.parse(body);
       var ims = jsonBody["ims"];
+      // return the ims
       resolve(ims);
     });
   });
 }
 
+
+// get the specifik users for a token (a channel)
 var getUsers = function(){
   return new Promise(function(resolve, reject){
     var groupString = 'https://slack.com/api/users.list?token=' + token;
@@ -91,6 +114,7 @@ var getUsers = function(){
 
       var jsonBody = JSON.parse(body);
       var users = jsonBody["members"];
+      // return the users
       resolve(users);
     });
   })
