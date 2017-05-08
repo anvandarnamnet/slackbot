d65d326928b1599e10b84461390c25668f25431d
Oskar Råhén <Oskar Rhn>
2017-05-08 16:20:45 +0200

    more scheduling

--- CronJobs.js
+++ CronJobs.js
@@ -5,18 +5,18 @@ var schedule = require('node-schedule');
 var cronJobs = new Map();
 
 // method when someone change a message (updates or deletes)
-var messageChanged = function(id){
-
+var messageChanged = function(id) {
+  
 }
 
 // start the cronjob
-var startCron = function(){
+var startCron = function() {
   var now = new Date();
   getMessageByDay(now.getDay())
 
   new CronJob('00 00 00 * * *', function() {
     for (var cronJob of cronJobs.values()) {
-        cronJob.stop();
+      cronJob.stop();
     }
     cronJobs = new Map();
 
@@ -27,9 +27,9 @@ var startCron = function(){
 }
 
 // get all the messages for a specifc day and schedule the messages
-var getMessageByDay = function(day){
-  jsonHandler.getMessageByDay(day).then(function(messages){
-    for(var i = 0; i < messages.length; i++){
+var getMessageByDay = function(day) {
+  jsonHandler.getMessageByDay(day).then(function(messages) {
+    for (var i = 0; i < messages.length; i++) {
       scheduleMessage(messages[i]);
     }
   });
@@ -39,41 +39,58 @@ var getMessageByDay = function(day){
 module.exports.start = startCron;
 
 // schedule a message with a cronjob
-var scheduleMessage = function(message){
+var scheduleMessage = function(message) {
+  // just add a method to the Date object; //you dont have to care about that :)
+  fixWeekNumberMethod();
 
   //KP DISCUSS!!!(SAMMA MEDDELANDE ELLER EJ?)
-  if(checkMessageShouldBeSend(message)){
-    var cron = new CronJob('00 ' + message.minute + ' ' + message.hour  +  ' * * *', function() {
-      jsonHandler.getMessageById(message.id).then(function(cb){
+  if (checkMessageShouldBeSend(message)) {
+    console.log("det händer!");
+    var cron = new CronJob('00 ' + message.minute + ' ' + message.hour + ' * * *', function() {
+      // get the newest version of the message object
+      jsonHandler.getMessageById(message.id).then(function(cb) {
         // the user has removed the 1on1
-        if(cb.length == 0){
+        if (cb.length == 0) {
           console.log("User removed 1 on 1");
           return;
         }
+        var updatedMessage = cb[0];
 
-          var updatedMessage = cb[0];
-          var users = updatedMessage.users;
-          for(var i = 0; i < users.length; i++){
-           apiHandler.sendDirectMessage(users[i].id, updatedMessage.message, updatedMessage.token).then(function(cb){
-              console.log("Direct message has been send");
-            });
-          }
-
-        });
-      }, null, true, 'Europe/Amsterdam');
-
-      cronJobs.set(message.id, cron);
+        var now = new Date();
+        var users = updatedMessage.users;
+        jsonHandler.messageHasBeenSend(updatedMessage, now.getWeek());
+        for (var i = 0; i < users.length; i++) {
+          apiHandler.sendDirectMessage(users[i].id, updatedMessage.message, updatedMessage.token).then(function(cb) {
+            console.log("Direct message has been send");
+          });
+        }
+      });
+    }, null, true, 'Europe/Amsterdam');
+    cronJobs.set(message.id, cron);
+  } else{
+    console.log("det händer inte");
   }
 }
 
 // check if message should be send depending on biweekly and so on.
-var checkMessageShouldBeSend = function(message){
-  var schouldBeSend = false;
+var checkMessageShouldBeSend = function(message) {
+  var now = new Date();
+  var week = now.getWeek();
+  var repeatsEvery = message.repeatsEvery;
 
-  // schould we d
-  jsonHandler.decreaseWeeksUntilMessage(message.id);
+  if (message.lastMessageSendWeek === week) {
+    return true;
+  }
+  if (message.lastMessageSendWeek === -1) {
+    return true;
+  }
+  if (week < message.lastMessageSendWeek) {
+    var numberOfWeeks = 52 - message.lastMessageSendWeek + week;
 
-  if(message.weeksUntilNewMessage === 1){
+    if (numberOfWeeks == repeatsEvery) {
+      return true;
+    }
+  } else if (now.getWeek() - message.lastMessageSendWeek == repeatsEvery) {
     return true;
   }
 
@@ -81,20 +98,33 @@ var checkMessageShouldBeSend = function(message){
 }
 
 // compare messages, make sure the message hasn't been changed
-var compareMessages = function(newMsg, oldMsg){
-  for(var i = 0; i < newMsg.users.length; i++){
-    if(newMsg.users[i].id != oldMsg.users[i].id){
+var compareMessages = function(newMsg, oldMsg) {
+  for (var i = 0; i < newMsg.users.length; i++) {
+    if (newMsg.users[i].id != oldMsg.users[i].id) {
       return false;
     }
   }
 
-  if(newMsg.hour != oldMsg.hour || newMsg.minute != oldMsg.minute){
+  if (newMsg.hour != oldMsg.hour || newMsg.minute != oldMsg.minute) {
     return false;
   }
 
-  if(newMsg.message !== oldMsg.message){
+  if (newMsg.message !== oldMsg.message) {
     return false;
   }
 
   return true;
 }
+
+var fixWeekNumberMethod = function() {
+    Date.prototype.getWeek = function() {
+      var date = new Date(this.getTime());
+      date.setHours(0, 0, 0, 0);
+      // Thursday in current week decides the year.
+      date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+      // January 4 is always in week 1.
+      var week1 = new Date(date.getFullYear(), 0, 4);
+      // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+      return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+    }
+  }
--- JsonHandeler.js
+++ JsonHandeler.js
@@ -3,22 +3,70 @@ var Mixpanel = require('mixpanel');
 var mixpanel = Mixpanel.init('28446a6b8950088604497db036de5bc2');
 
 var messageSchema = mongoose.Schema({
-  token: {type: String, required: true},
-  teamInfo: {type: Object, required: true},
-  repeatsEvery:{type:Number, required:true},
-  weeksUntilNewMessage: {type:Number, required:true},
-  hour: {type: String, required: true},
-  minute: {type: String, required: true},
-  monday: {type: Boolean, required: true},
-  tuesday: {type: Boolean, required: true},
-  wednesday: {type: Boolean, required: true},
-  thursday: {type: Boolean, required: true},
-  friday: {type: Boolean, required: true},
-  saturday: {type: Boolean, required: true},
-  sunday: {type: Boolean, required: true},
-  message: {type: String, required: true},
-  users: {type: Object, required: true},
-  createdAt: {type: Date, default:Date.now}
+  token: {
+    type: String,
+    required: true
+  },
+  teamInfo: {
+    type: Object,
+    required: true
+  },
+  repeatsEvery: {
+    type: Number,
+    required: true
+  },
+  lastMessageSendWeek: {
+    type: Number,
+    default: -1
+  },
+  hour: {
+    type: String,
+    required: true
+  },
+  minute: {
+    type: String,
+    required: true
+  },
+  monday: {
+    type: Boolean,
+    required: true
+  },
+  tuesday: {
+    type: Boolean,
+    required: true
+  },
+  wednesday: {
+    type: Boolean,
+    required: true
+  },
+  thursday: {
+    type: Boolean,
+    required: true
+  },
+  friday: {
+    type: Boolean,
+    required: true
+  },
+  saturday: {
+    type: Boolean,
+    required: true
+  },
+  sunday: {
+    type: Boolean,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  users: {
+    type: Object,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 // model our schema and make it available
@@ -26,15 +74,17 @@ var messages = mongoose.model("messages", messageSchema);
 module.exports = messages;
 
 // get all messages for a certain token
-var getMessagesByToken = function(token){
-  return new Promise(function(resolve, reject){
-    var query = messages.find({token: token});
+var getMessagesByToken = function(token) {
+  return new Promise(function(resolve, reject) {
+    var query = messages.find({
+      token: token
+    });
 
-    query.exec(function(err, messages){
-     if(err){
-       return console.log(err);
-     }
-     resolve(messages);
+    query.exec(function(err, messages) {
+      if (err) {
+        return console.log(err);
+      }
+      resolve(messages);
     });
   });
 }
@@ -43,63 +93,63 @@ module.exports.getMessagesByToken = getMessagesByToken;
 
 
 // get all the messages for a specific day
-var getMessageByDay = function(day){
+var getMessageByDay = function(day) {
 
-  return new Promise(function(resolve, reject){
+  return new Promise(function(resolve, reject) {
     var query;
     switch (day) {
       case 0:
-      query = messages.find({
-        sunday: true
-      });
-      break;
+        query = messages.find({
+          sunday: true
+        });
+        break;
 
       case 1:
-      query = messages.find({
-        monday: true
-      });
-      break;
+        query = messages.find({
+          monday: true
+        });
+        break;
 
       case 2:
-      query = messages.find({
-        tuesday: true
-      });
-      break;
+        query = messages.find({
+          tuesday: true
+        });
+        break;
 
       case 3:
-      query = messages.find({
-        wednesday: true
-      });
-      break;
+        query = messages.find({
+          wednesday: true
+        });
+        break;
 
       case 4:
-      query = messages.find({
-        thursday: true
-      });
-      break;
+        query = messages.find({
+          thursday: true
+        });
+        break;
 
       case 5:
-      query = messages.find({
-        friday: true
-      });
-      break;
+        query = messages.find({
+          friday: true
+        });
+        break;
 
       case 6:
-      query = messages.find({
-        saturday: true
-      });
-      break;
+        query = messages.find({
+          saturday: true
+        });
+        break;
 
       default:
-      query = messages.find();
+        query = messages.find();
     }
 
-    query.exec(function(err, messages){
-      if(err){
+    query.exec(function(err, messages) {
+      if (err) {
         reject(err);
-      } else{
-          resolve(messages);
-        }
+      } else {
+        resolve(messages);
+      }
     });
 
   });
@@ -108,20 +158,20 @@ var getMessageByDay = function(day){
 module.exports.getMessageByDay = getMessageByDay;
 
 
-var getMessagesByTime = function(startTime, endTime){
+var getMessagesByTime = function(startTime, endTime) {
 
-  return new Promise(function(resolve,reject){
+  return new Promise(function(resolve, reject) {
     var query = messages.find({
       hour: {
-          $gt: startTime,
-          $lt: endTime
-        }
+        $gt: startTime,
+        $lt: endTime
+      }
     });
 
-    query.exec(function(err, messages){
-      if(err){
+    query.exec(function(err, messages) {
+      if (err) {
         reject(err);
-      }else{
+      } else {
         resolve(messages);
       }
     })
@@ -130,16 +180,16 @@ var getMessagesByTime = function(startTime, endTime){
 
 module.exports.getMessagesByTime = getMessagesByTime;
 
-var getMessageById = function(id){
-  return new Promise(function(resolve,reject){
+var getMessageById = function(id) {
+  return new Promise(function(resolve, reject) {
     var query = messages.find({
-      _id:id
+      _id: id
     });
 
-    query.exec(function(err, messages){
-      if(err){
+    query.exec(function(err, messages) {
+      if (err) {
         reject(err);
-      }else{
+      } else {
         resolve(messages);
       }
     })
@@ -149,47 +199,49 @@ var getMessageById = function(id){
 module.exports.getMessageById = getMessageById;
 
 
-var decreaseWeeksUntilMessage = function(id){
-  getMessageById(id).then(function(message){
-    console.log(message);
-
-  });
+var messageHasBeenSend = function(message, weekSend) {
+  messages.findByIdAndUpdate(
+    message.id, {
+      "lastMessageSendWeek": weekSend
+    },
+    function(err, document) {
+      console.log(err);
+    });
 }
 
-module.exports.decreaseWeeksUntilMessage = decreaseWeeksUntilMessage;
+module.exports.messageHasBeenSend = messageHasBeenSend;
 
 
 // add a new message
-var addNewMessage = function(token, teaminfoInput, users,hour, minute, message, days, repeatsEvery){
+var addNewMessage = function(token, teaminfoInput, users, hour, minute, message, days, repeatsEvery) {
 
-  return new Promise(function(resolve, reject){
+  return new Promise(function(resolve, reject) {
     var newMessage = {
       token: token,
       teamInfo: teaminfoInput,
-      repeatsEvery:repeatsEvery,
-      weeksUntilNewMessage: 1,
+      repeatsEvery: repeatsEvery,
       hour: hour,
       minute: minute,
       monday: days.monday,
-      tuesday:days.tuesday,
-      wednesday:days.wednesday,
-      thursday:days.thursday,
-      friday:days.friday,
-      saturday:days.saturday,
-      sunday:days.sunday,
+      tuesday: days.tuesday,
+      wednesday: days.wednesday,
+      thursday: days.thursday,
+      friday: days.friday,
+      saturday: days.saturday,
+      sunday: days.sunday,
       message: message,
       users: users
     }
-    
+
     var uploadMessages = messages(newMessage);
 
-    uploadMessages.save(function(err){
-      if(err){
+    uploadMessages.save(function(err) {
+      if (err) {
         reject("Something went wrong when adding a new message to the db");
         mixpanel.track('ERROR_adding_1on1_DB');
 
         console.log("Something went wrong when adding a new message to the db: " + err);
-      } else{
+      } else {
         resolve("Message added");
         mixpanel.track('new_1on1');
 
