41b166aa1bb0c89ee870787482bffe629f70916a
unknown <rahlenoskar@gmail.com>
2017-07-30 20:16:03 +0200

    bodyparser

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ mongoose.connect("mongodb://oskar:oskar@ds157809.mlab.com:57809/slackbot");
 
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.configure(function(){
+  app.use(express.bodyParser());
+});
 app.use(cookieParser());
 
 app.set('port', (process.env.PORT || 5000));
