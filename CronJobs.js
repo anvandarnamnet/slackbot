3217a38b80c27b46c9803af9763884f49c4af325
Oskar Råhlén <rahlenoskar@gmail.com>
2017-08-23 21:08:00 +0200

    more logging

--- CronJobs.js
+++ CronJobs.js
@@ -62,6 +62,7 @@ var scheduleMessage = function(message) {
              messageQueue.getMessageQueue(updatedMessage.teamInfo.team.id, users[i].id).then(function(queue){
                  if(id !== 'USLACKBOT'){
                    if(queue[0].messageQueue.length === 0){
+                     console.log("cronjob sending dm")
                    apiHandler.sendDirectMessage(id, updatedMessage.message[0], updatedMessage.token).then(function(cb) {});
                    var newMessages = []
                    if(updatedMessage.message.length > 1){
