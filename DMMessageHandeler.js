c7e6d8984f12e76ddb8bf79a9821c4fde0e58553
rahlenoskar@gmail.com <rahlenoskar@gmail.com>
2017-09-02 22:26:10 +0200

    asdasd

--- DMMessageHandeler.js
+++ DMMessageHandeler.js
@@ -135,6 +135,7 @@ var getMessagesQueueFromChannel = function(teamId, userId, channel){
     return new Promise(function(resolve, reject) {
         var s = {
             teamId: teamId,
+            userId: userId,
             imId:channel
 
         }
